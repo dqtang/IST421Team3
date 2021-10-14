e584a285d0933acb465347631ce328441378cd50
dqtang <dqt5211@psu.edu>
2021-10-13 20:18:26 -0400

    DBFunction Base Created

---
+++ DBConnect.php
@@ -0,0 +1,14 @@
+
+<?php
+function DBCredential(){
+
+    //DB Credential Info
+    $servername="localhost";
+    $username ="root";
+    $password = "";
+    $dbname ="flexmedia";
+
+    return array('servername'=>$servername, 'username'=>$username, 'password'=>$password, 'dbname'=>$dbname);
+
+}
+?>
