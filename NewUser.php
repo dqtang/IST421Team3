16fcd34a0d9e77c261c1b1aeec2eda00663ac2c8
dqtang <dqt5211@psu.edu>
2021-11-09 22:47:07 -0500

    NewUser Redirect w/ id_token

--- NewUser.php
+++ NewUser.php
@@ -1,28 +1,30 @@
 <?php 
 
-    include 'DBConnect.php'();
+      include 'DBConnect.php';
 
-    $credentials = DBCredential();
-    extract($credentials);
-
-
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $email = $_POST["email"];
-    $DoB = $_POST["DoB"];
-    $phoneNumber = $_POST["phoneNumber"];
+      $credentials = DBCredential();
+      extract($credentials);
 
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+      $firstName = $_POST["firstName"];
+      $lastName = $_POST["lastName"];
+      $email = $_POST["email"];
+      $DoB = $_POST["DoB"];
+      $phoneNumber = $_POST["phoneNumber"];
+      $gID = $_POST["gID"];
+      $profileImageUrl = $_POST["profileImageUrl"];
+      $id_token = $_POST["id_token"];
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
       }else{
-            $sql = "INSERT INTO profile_information (First_Name, Last_Name, Phone_Number, DOB, Email) VALUES ('$firstName', '$lastName', '$phoneNumber','$DoB','$email')";
+            $sql = "INSERT INTO profile_information (First_Name, Last_Name, Phone_Number, DOB, Email, Profile_ID, Profile_URL) VALUES ('$firstName', '$lastName', '$phoneNumber','$DoB','$email', '$gID', '$profileImageUrl')";
 
             if ($conn->query($sql) === TRUE){
-                  echo "New record created successfully";
-
+                  header("Location: http://localhost/IST 421/Homepage.html?id_token=".$id_token);
+                  exit();
 
             }else{
                   echo"Error: " .$sql ."<br>" . $conn->error;
