a22f4a1a68c4f16b18cbc6ab10b18cbfa19fca4d
dqtang <dqt5211@psu.edu>
2021-12-01 02:42:27 -0500

    Moved Comment

--- gVerify.php
+++ gVerify.php
@@ -4,11 +4,11 @@ require_once 'vendor/autoload.php';
 
 // Get $id_token via HTTPS POST.
 
-
+// Specify the CLIENT_ID of the app that accesses the backend
 $id_token = $_POST['id_token'];
 $CLIENT_ID = "6324381213-tn0ffjmb6r1s6ufa514ue4ql7tdr3o0k.apps.googleusercontent.com";
 
-$client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+$client = new Google_Client(['client_id' => $CLIENT_ID]);  
 $payload = $client->verifyIdToken($id_token);
 if ($payload) {
   $userid = $payload['sub'];
