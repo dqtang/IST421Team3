e8212a0d2bcda80b4c2aace2fcea7747b80deec1
dqtang <dqt5211@psu.edu>
2021-12-01 02:42:48 -0500

    UserActivity Profile Load

---
+++ Profile.php
@@ -0,0 +1,41 @@
+<?php 
+
+      include 'DBConnect.php';
+
+      $credentials = DBCredential();
+      extract($credentials);
+
+
+      $profileID = $_POST["profileID"];
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+      }else{
+            $sql = "SELECT post.Picture, post.Profile_ID, post.TimeStamp, Names.First_Name, Names.Last_Name, Names.Profile_URL FROM post_information as post 
+                        INNER JOIN profile_information as Names ON $profileID = Names.Profile_ID 
+                              Order BY TimeStamp DESC";
+
+            $queryResult = $conn->query($sql);
+
+            if ($queryResult->num_rows>0){
+              while ($row = $queryResult->fetch_assoc()){
+
+                //Adds query results into an array
+                  $recentPosts[] = $row;
+              } 
+              //echos array in json                  
+              echo json_encode($recentPosts);
+
+          }
+
+      }
+
+
+
+      $conn->close();
+
+
+
+
+?>
