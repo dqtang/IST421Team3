5dc0e2168c11d671dcc73d32fb6214bd9ec6cf3a
dqtang <dqt5211@psu.edu>
2021-12-02 15:23:00 -0500

    Added Like counter per post

--- php/Profile.php
+++ php/Profile.php
@@ -12,7 +12,7 @@
       if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
       }else{
-            $sql = "SELECT post.Picture, post.Profile_ID, post.TimeStamp, Names.First_Name, Names.Last_Name, Names.Profile_URL FROM post_information as post 
+            $sql = "SELECT post.Picture, post.Profile_ID, post.Likes, post.TimeStamp, Names.First_Name, Names.Last_Name, Names.Profile_URL FROM post_information as post 
                         INNER JOIN profile_information as Names ON $profileID = Names.Profile_ID 
                               Order BY TimeStamp DESC";
 
